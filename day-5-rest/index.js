db81c71240e8798e890776b73de50befcf2bc7f6
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-01 16:52:01 +0530

    setup rest posts app

---
+++ day-5-rest/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
+const app = express();
+const PORT = 3000;
+
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "kiran",
+    content: "I am learning REST backend."
+  },
+  {
+    id: uuidv4(),
+    username: "rahul",
+    content: "Express makes routing easier."
+  }
+];
+
+app.get("/", (req, res) => {
+  res.redirect("/posts");
+});
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`Server Listening on PORT ${PORT}`);
+})
