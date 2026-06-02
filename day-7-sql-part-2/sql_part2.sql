42a47398d366d226d649f0afbfb07bd8f1554217
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-02 08:56:08 +0530

    practice where clause queries

---
+++ day-7-sql-part-2/sql_part2.sql
@@ -0,0 +1,45 @@
+-- Day 36 SQL Part 2
+-- Advanced SQL practice
+-- Practiced by Kiran
+
+CREATE DATABASE IF NOT EXISTS college_part2;
+
+USE college_part2;
+
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  age INT,
+  course VARCHAR(50),
+  marks INT,
+  city VARCHAR(50)
+);
+
+INSERT INTO students
+(id, name, age, course, marks, city)
+VALUES
+(1, 'Kiran', 22, 'CSE', 85, 'Bengaluru'),
+(2, 'Rahul', 21, 'ECE', 72, 'Mysuru'),
+(3, 'Sneha', 23, 'CSE', 91, 'Bengaluru'),
+(4, 'Asha', 24, 'MECH', 65, 'Tumakuru'),
+(5, 'Rohit', 22, 'CSE', 45, 'Bengaluru'),
+(6, 'Priya', 21, 'ECE', 88, 'Mysuru'),
+(7, 'Manoj', 25, 'MECH', 55, 'Hubballi'),
+(8, 'Divya', 22, 'CIVIL', 78, 'Bengaluru');
+
+SELECT * FROM students;
+
+SELECT * FROM students
+WHERE course = 'CSE';
+
+SELECT * FROM students
+WHERE marks > 80;
+
+SELECT * FROM students
+WHERE city = 'Bengaluru';
+
+SELECT name, marks FROM students
+WHERE marks >= 75;
