33a73a8aeea6aad484d9b812a1ac392be292e00f
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-02 08:17:14 +0530

    create students table

--- day-6-sql-basics/college.sql
+++ day-6-sql-basics/college.sql
@@ -5,3 +5,18 @@ CREATE DATABASE college;
 
 USE college;
 
+SELECT DATABASE();
+
+SHOW TABLES;
+
+CREATE TABLE students (
+  id INT,
+  name VARCHAR(50),
+  age INT,
+  course VARCHAR(50)
+);
+
+SHOW TABLES;
+
+DESC students;
+
