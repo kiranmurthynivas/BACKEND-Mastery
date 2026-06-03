51b9e00c540f70d1c1508f313e6f879a290bb9d7
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-03 12:38:01 +0530

    insert multiple mongodb documents

--- day-9-mongodb-basics/mongodb_basics.js
+++ day-9-mongodb-basics/mongodb_basics.js
@@ -6,3 +6,34 @@ db.students.insertOne({
   course: "CSE",
   city: "Bengaluru"
 });
+
+db.students.insertMany([
+  {
+    name: "Rahul",
+    age: 21,
+    course: "ECE",
+    city: "Mysuru",
+    marks: 72
+  },
+  {
+    name: "Sneha",
+    age: 23,
+    course: "CSE",
+    city: "Bengaluru",
+    marks: 91
+  },
+  {
+    name: "Asha",
+    age: 24,
+    course: "MECH",
+    city: "Tumakuru",
+    marks: 65
+  },
+  {
+    name: "Priya",
+    age: 21,
+    course: "ECE",
+    city: "Mysuru",
+    marks: 88
+  }
+]);
