f3d03d861a84f5ceba667851a573568f26d86f4c
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2026-06-03 06:33:30 +0530

    practice group by queries

--- day-7-sql-part-2/sql_part2.sql
+++ day-7-sql-part-2/sql_part2.sql
@@ -88,3 +88,33 @@ SELECT * FROM students
 ORDER BY marks ASC
 LIMIT 2;
 
+SELECT COUNT(*) AS total_students
+FROM students;
+
+SELECT AVG(marks) AS average_marks
+FROM students;
+
+SELECT MAX(marks) AS highest_marks
+FROM students;
+
+SELECT MIN(marks) AS lowest_marks
+FROM students;
+
+SELECT SUM(marks) AS total_marks
+FROM students;
+
+SELECT course, COUNT(*) AS total_students
+FROM students
+GROUP BY course;
+
+SELECT city, COUNT(*) AS total_students
+FROM students
+GROUP BY city;
+
+SELECT course, AVG(marks) AS average_marks
+FROM students
+GROUP BY course;
+
+SELECT city, MAX(marks) AS highest_marks
+FROM students
+GROUP BY city;
